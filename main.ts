662e7c5190a70381cc457336d3608377b0debff1
deisarae11 <146109987+deisarae11@users.noreply.github.com>
2023-09-26 10:10:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let raerae = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 3 3 3 3 3 3 3 . . . . . 
+    . . b b b b b b b b b b b . . . 
+    . . b b a a a b a a a b b . . . 
+    . . b b a b a b a b a b b . . . 
+    . . b b a a a b a a a b b . . . 
+    . . b b b b b b b b b b b . . . 
+    . . b b b b b b b b b b b . . . 
+    . . b b b b a a a b b b b . . . 
+    . . b b b b b b b b b b b . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
